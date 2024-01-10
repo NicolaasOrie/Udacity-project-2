d87065c60a3e6ee448e07d42e60e7ec46384671e
NicolaasOrie <65601020+NicolaasOrie@users.noreply.github.com>
2024-01-10 16:00:16 +0100

    replace classfier trainer

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -22,12 +22,16 @@ from sklearn.multioutput import MultiOutputClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.compose import ColumnTransformer
+from sklearn.base import TransformerMixin, BaseEstimator
 
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_auc_score
 from sklearn.metrics import roc_auc_score, auc
 
-## HIER WAS JE GEBLEVEN ! CODE IN DEF LOAD_DATA IS GEKOPIEERD VAN DE OEFEN SPACE EN MOET NU KLOPPEND GEMAAKT WORDEN
+import spacy
+#spacy.cli.download("en_core_web_lg")
+nlp = spacy.load("en_core_web_lg")
 
 def load_data(database_filepath):
     """ input : filepath to database
@@ -50,7 +54,6 @@ def tokenize(text):
     """ input : sentence (string)
         output : tokenized sentence (list)"""
     stemmer = PorterStemmer()
-    
     tokens = word_tokenize(text)
     
     # Remove stopwords
@@ -62,39 +65,58 @@ def tokenize(text):
     return stemmed_tokens
 
 
-
+   
 def build_model(X_train, y_train):
     """ input : X_train, y_train
         output : ML model (fitted SK learn pipeline)"""
-    # Create a text vectorizer for 'message' column
-    text_vectorizer = TfidfVectorizer(max_features=1000)
+
     # Create a standard scaler for 'genre' column
     genre_scaler = StandardScaler(with_mean=False)
     # Create a multi-output classifier (Random Forest, for example)
-    classifier = MultiOutputClassifier(RandomForestClassifier(n_estimators=200, max_depth=50, random_state=42, n_jobs=5))
+    classifier = MultiOutputClassifier(RandomForestClassifier(n_estimators=2, max_depth=2, random_state=42, n_jobs=1))
     #create MeanAmbeddingVectorizer
-    from gensim.models.word2vec import Word2Vec
-    model = Word2Vec(X, size=100, window=5, min_count=5, workers=2)
-    model.wv.index2word
-    w2v = {w: vec for w, vec in zip(model.wv.index2word, model.wv.syn0)}
-    class MeanEmbeddingVectorizer(object):
-        def __init__(self, word2vec):
-            self.word2vec = word2vec
-            self.dim = len(next(iter(word2vec.values())))
-    def fit(self, X, y):
-        return self
-    def transform(self, X):
-        return np.array([np.mean([self.word2vec[w] for w in words if w in self.word2vec]
-                         or [np.zeros(self.dim)], axis=0)
-                         for words in X
-                         ])
- 
+    
+    column_preprocessor = ColumnTransformer(
+    [
+        ('text_glove', GloveVectorTransformer(nlp), 'text'),
+    ],
+    remainder='drop',
+    n_jobs=1)
+
     # Create a pipeline that processes 'message'column and predicts the output columns
     pipeline = Pipeline([
-    (("word2vec vectorizer", MeanEmbeddingVectorizer(w2v))),
+    ('column_preprocessor', column_preprocessor),
     ('genre_scaler', genre_scaler),
-    ('classifier', classifier)
-    ])
+    ('classifier', classifier)])
+   
+          
+    # Define the hyperparameter grid for RandomForestClassifier
+    param_grid = {
+    'classifier__estimator__n_estimators'   : [5, 20, 50],
+    'classifier__estimator__max_depth'      : [5, 10, 20]
+    }
+
+    # Create a grid search object
+    grid_search = GridSearchCV(pipeline, param_grid, cv=3, n_jobs=1)
+
+    print('start grid search')
+    
+    # Fit the grid search to the training data
+    grid_search.fit(X_train, y_train)
+
+    print('results grid search:')
+    #Get the best parameters and estimator from the grid search
+    best_params = grid_search.best_params_
+    best_estimator = grid_search.best_estimator_
+
+    # Print the best parameters
+    print("Best Parameters:")
+    print(best_params)
+    
+    # Print the best estimator (pipeline with optimized hyperparameters)
+    print("Best Estimator:")
+    print(best_estimator)
+
     pipeline.fit(X_train, y_train)
     return pipeline
 
@@ -124,22 +146,57 @@ def save_model(model, model_filepath):
     joblib.dump(model, model_filepath)
     pass
 
+from gensim.models.word2vec import Word2Vec
+
+class GloveVectorTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, nlp):
+        self.nlp = nlp
+        self.dim = 300
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return np.array([self.nlp(text).vector for text in X])  
+    
+# Example list of strings (similar to X_train)
+print('##################################################')
+example_texts = [
+    "This is an example sentence.",
+    "Another example for testing.",
+    "Let's see if it works properly."
+]
+
+# Create an instance of the GloveVectorTransformer
+glove_transformer = GloveVectorTransformer(nlp)
+
+# Transform the example_texts using the transformer
+transformed_data = glove_transformer.transform(example_texts)
+
+print("Transformed Data Shape:", transformed_data.shape)
+print("Transformed Data:")
+print(transformed_data)
 
 def main():
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         print(database_filepath)
+        
         X, Y = load_data(database_filepath)
         X = X.apply(tokenize)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+        
         X_train = [' '.join(inner_list) for inner_list in X_train]
-        X_test = [' '.join(inner_list) for inner_list in X_test]
+        X_test  = [' '.join(inner_list) for inner_list in X_test]
+        X_train = pd.DataFrame({'text': X_train})
+        X_test = pd.DataFrame({'text': X_test})
+
         print('Building model...')
         model = build_model(X_train, Y_train)
         
-        print('Training model...')
-        model.fit(X_train, Y_train)
+        #print('Training model...')
+        #model.fit(X_train, Y_train)
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test)
